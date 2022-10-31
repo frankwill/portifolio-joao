741bf9ec4c893c12cfd8c8dfbeb4b6360ee43de0
Frank <frank.will999@gmail.com>
2022-10-30 22:07:39 -0300

    side menu finished

---
+++ assets/js/menuLateral.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector('.header__menu')
+const menuLateral = document.querySelector('.mobile__menu')
+
+botao.addEventListener('click', () => {
+    menuLateral.classList.toggle('open')
+    botao.classList.toggle('header__menu--open')
+})
